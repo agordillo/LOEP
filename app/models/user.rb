9c107fe9bc294df5550cd38fe0ed36858c732b05
Aldo Gordillo <agordillos@gmail.com>
2013-12-05 19:55:00 +0100

    More devise configuration. Allow users to login using either their name or email. Change redirect after bad login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,28 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :roles
 
+  validates :name,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  attr_accessible :login
+
+  ### This is the correct devise method i override with the code below
+  ### def self.find_for_database_authentication(warden_conditions)
+  ### end
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 
 
 
-  
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,6 +30,7 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [ :email ]
+  config.authentication_keys = [ :login ]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
@@ -238,6 +239,10 @@ Devise.setup do |config|
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
 
+  config.warden do |manager|
+    manager.failure_app = CustomFailure
+  end
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
@@ -251,4 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
 end
