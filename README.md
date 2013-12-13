20e5064c6b8291e59c48e5321e6c0ef4f22b474e
Aldo Gordillo <agordillos@gmail.com>
2013-12-13 14:48:37 +0100

    Update readme

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Installation:
 
 1. bundle install
 2. Rename config/initializers/secret_token_example.rb to config/initializers/secret_token.rb and execute 'rake secret' to generate a new secret_token and copy it to config/initializers/secret_token.rb
-3. Rename config/initializers/recaptcha_example.rb to config/initializers/recaptcha.rb , and get new keys at https://www.google.com/recaptcha
-4. Make migrations rake db:migrate
-5. Populate db rake db:populate
+3. Rename config/initializers/secret_token_example.rb to config/initializers/secret_token.rb and execute 'rake secret' to generate a new secret_token and copy it to config/initializers/secret_token.rb
+4. Rename config/database_example.yml to config/database.yml , and write conf
+5. Make migrations rake db:migrate
+6. Populate db rake db:populate
 rails s and enjoy!
---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# set :application, "set your application name here"
+# set :repository,  "set your repository location here"
+
+# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# # if you want to clean up old releases on each deploy uncomment this:
+# # after "deploy:restart", "deploy:cleanup"
+
+# # if you're still using the script/reaper helper you will need
+# # these http://github.com/rails/irs_process_scripts
+
+# # If you are using Passenger mod_rails uncomment this:
+# # namespace :deploy do
+# #   task :start do ; end
+# #   task :stop do ; end
+# #   task :restart, :roles => :app, :except => { :no_release => true } do
+# #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+# #   end
+# # end
+
+require 'bundler/capistrano' # for bundler support
+
+set :application, "LOEP"
+set :repository,  "git@github.com:agordillo/LOEP.git"
+
+set :user, 'loep'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+role :web, "loep.global.dit.upm.es"                          # Your HTTP server, Apache/etc
+role :app, "loep.global.dit.upm.es"                          # This may be the same as your `Web` server
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
