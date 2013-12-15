7ea5421406491419a8f690dfb8e5746397b1f53a
Aldo Gordillo <agordillos@gmail.com>
2013-12-15 22:01:18 +0100

    Best-effort matching

--- app/models/matching_system.rb
+++ app/models/matching_system.rb
@@ -68,7 +68,70 @@ class MatchingSystem
   # Prioritize workload balancing: Best-effort Matching
   # Same LOs for each reviewer, try to choose the most appropriate LO for each reviewer
   def self.pwlBestEffort(nepl,los,reviewers,assignmentParams)
-    return nil
+    assignments = [];
+
+    reviewerLOmatchings = Hash.new
+    # reviewerLOmatchings["reviewerId"] = ["loId1","loId2", ...]
+
+    loNassignments = Hash.new
+    # loNassignments["loId"] = nAssignments
+
+    amatrix = Hash.new;
+    # Appropriateness Matrix amatrix["reviewerId"]["loId"] = MatchingScore(lo,reviewer)
+
+    reviewers.each do |reviewer|
+      reviewer_matrix = Hash.new;
+       # reviewer_matrix["loId"] = MatchingScore(lo,reviewer)
+      los.each do |lo|
+        reviewer_matrix[lo.id] = getMatchingScore(lo,reviewer)
+      end
+      amatrix[reviewer.id] = reviewer_matrix
+    end
+
+    reviewers.shuffle
+
+    if nepl > reviewers.length
+      #Is not possible
+      #A reviewer can review a LO only once
+      nepl = reviewers.length
+    end
+
+    while(los.length != 0)
+      reviewers.each do |reviewer|
+          if reviewerLOmatchings[reviewer.id].nil?
+            reviewerLOmatchings[reviewer.id] = [];
+          end
+
+          loIdToReview = amatrix[reviewer.id].sort {|a1,a2| a2[1]<=>a1[1]}.first[0]
+          loToReview = Lo.find(loIdToReview)
+
+          #Remove LO from Reviewers matrix
+          amatrix[reviewer.id].delete loToReview.id
+
+          if !reviewerLOmatchings[reviewer.id].include?(loToReview.id)
+            assignments << match(loToReview,reviewer,assignmentParams)
+
+            reviewerLOmatchings[reviewer.id].push(loToReview.id);
+            if loNassignments[loToReview.id].nil?
+              loNassignments[loToReview.id] = 0
+            end
+            loNassignments[loToReview.id] = loNassignments[loToReview.id] + 1
+
+            if loNassignments[loToReview.id] >= nepl
+              #Remove LO from all hashes
+              los = los.reject { |lo| lo.id == loToReview.id }
+              reviewers.each do |reviewer|
+                amatrix[reviewer.id].delete loToReview.id
+              end
+              if los.length === 0
+                break
+              end
+            end
+          end
+      end
+    end
+
+    return assignments
   end
 
   # Prioritize reviewer appropriateness
@@ -86,9 +149,38 @@ class MatchingSystem
     return as
   end
 
-  #A reviewer shouldn't review a LO more than once
-  def self.isRepeatedAssignment(assignments,lo,reviewer)
+  def self.getMatchingScore(lo,reviewer)
+    score = 0
+    score = score + getLanguageScore(lo,reviewer)
+    score = score + getTagsSimilarityScore(lo,reviewer)
+    return score
+  end
 
+  def self.getLanguageScore(lo,reviewer)
+    lanScore = 0
+    if lo.language === reviewer.language
+      #Preferred or Native language match
+      lanScore = lanScore + 1000
+    elsif reviewer.languages.include? lo.language
+      # LO is an understandable language for the reviewer
+      lanScore = lanScore + 500
+    else
+      # Reviewer may not understand the LO
+      lanScore = lanScore - 500
+    end
+    lanScore
+  end
+
+  def self.getTagsSimilarityScore(lo,reviewer)
+    tagScore = 0
+    
+    reviewer.tags.each do |tag|
+      if lo.tags.include? tag
+        tagScore = tagScore + 150
+      end
+    end
+
+    tagScore
   end
 
 end
