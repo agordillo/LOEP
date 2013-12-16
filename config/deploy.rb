e51db855616724c4a226ad6a6d78603260a870d7
Aldo Gordillo <agordillos@gmail.com>
2013-12-16 10:22:56 +0100

    Deploy.rb update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,3 @@
-# set :application, "set your application name here"
-# set :repository,  "set your repository location here"
-
-# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# # if you want to clean up old releases on each deploy uncomment this:
-# # after "deploy:restart", "deploy:cleanup"
-
-# # if you're still using the script/reaper helper you will need
-# # these http://github.com/rails/irs_process_scripts
-
-# # If you are using Passenger mod_rails uncomment this:
-# # namespace :deploy do
-# #   task :start do ; end
-# #   task :stop do ; end
-# #   task :restart, :roles => :app, :except => { :no_release => true } do
-# #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-# #   end
-# # end
-
 require 'bundler/capistrano' # for bundler support
 
 set :application, "LOEP"
@@ -41,16 +15,46 @@ role :web, "loep.global.dit.upm.es"                          # Your HTTP server,
 role :app, "loep.global.dit.upm.es"                          # This may be the same as your `Web` server
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# before 'deploy:assets:precompile', 'deploy:link_files'
+after 'deploy:create_symlink', 'deploy:link_files'
+after 'deploy:restart', 'deploy:precompile_assets'
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+ # Tasks for passenger mod_rails
  task :start do ; end
  task :stop do ; end
  task :restart, :roles => :app, :except => { :no_release => true } do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
  end
+
+ #Other tasks
+  task :fix_file_permissions do
+  end
+
+  task :link_files do
+    puts "LINK FILES DO"
+    run "ln -s #{ shared_path}/secret_token.rb #{ release_path }/config/initializers/secret_token.rb"
+    run "rm  #{ release_path }/config/initializers/session_store.rb"
+    run "ln -s #{ shared_path}/secret_token.rb #{ release_path }/config/initializers/session_store.rb"
+    run "ln -s #{ shared_path}/recaptcha.rb #{ release_path }/config/initializers/recaptcha.rb"
+    run "ln -s #{ shared_path}/database.yml #{ release_path }/config/database.yml"
+  end
+
+  task :precompile_assets do
+    run "cd #{ release_path } && bundle exec \"rake assets:precompile --trace RAILS_ENV=production\""
+  end
+
+  # task :start_sphinx do
+  #   run "cd #{ current_path } && kill -9 `cat log/searchd.production.pid` || true"
+  #   run "cd #{ release_path } && bundle exec \"rake ts:rebuild RAILS_ENV=production\""
+  # end
+
+  # task :fix_sphinx_file_permissions do
+  #   run "/bin/chmod g+rw #{ release_path }/log/searchd*"
+  #   sudo "/bin/chgrp www-data #{ release_path }/log/searchd*"
+  # end
+
 end
