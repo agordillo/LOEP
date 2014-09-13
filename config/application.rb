fd1bc52c453882b209f4355b5903934806f84276
Aldo Gordillo <agordillos@gmail.com>
2014-09-13 12:20:58 +0200

    Add Rake task to find missing translations

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module LOEP
     # rails will fallback to config.i18n.default_locale translation
     config.i18n.fallbacks = true
     # rails will fallback to en, no matter what is set as config.i18n.default_locale
-    config.i18n.fallbacks = [:en]
+    # config.i18n.fallbacks = [:en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
