4f5893b71840acb5e917a514a3c3c633af03177a
agordillo <agordillos@gmail.com>
2018-03-08 18:19:20 +0100

    Minor changes on loep_stars plugin

---
+++ loep_plugins/loep_stars/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '73557f508ffe44024e115063241972c1908f7bf087dfb839a1e67b02bf24acf14010905f05dbd130911a439f861bc3d24839ba0735d7f57860dbfbd705756b18'
---
+++ loep_plugins/loep_stars/test/loep_stars_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LoepStarsTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, LoepStars
+  end
+end
